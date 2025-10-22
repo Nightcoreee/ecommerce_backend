43ca2772a345f702e5f68596189d3580e6fca891
Ngoc Ha <ha69630@gmail.com>
2025-10-22 23:48:42 +0700

    add morgan

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 //init db
 
 //init handle error
 
-//init 
+//init middleware
+app.use(morgan('dev'));
+// app.use(morgan('compile'));
+// app.use(morgan('common'));
+// app.use(morgan('short'));
+// app.use(morgan('tiny'));
+
+//init routes
+app.get('/', (req, res, next) => {
+    res.status(200).json({ 
+        message: 'Hello World' 
+    });
+})
 
 
 module.exports = app;
