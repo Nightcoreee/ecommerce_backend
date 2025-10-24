10664a69657db4f2564675f26192219ccb88f8cd
Ngoc Ha <ha69630@gmail.com>
2025-10-24 23:44:49 +0700

    Connect mongoose

--- src/app.js
+++ src/app.js
@@ -5,20 +5,22 @@ const compression = require('compression');
 const app = express();
 
 //init db
-
+require('./dbs/init.mongodb.lv0');
 //init handle error
 
-//init middleware
+//INIT MIDDLEWARES
 // 'dev' dùng để log chi tiết các request trong quá trình phát triển
 app.use(morgan('dev'));
 // Tăng cường bảo mật ứng dụng bằng cách thiết lập các header HTTP
 app.use(helmet());
+// compression middleware để nén các phản hồi HTTP, giúp giảm kích thước dữ liệu truyền tải và cải thiện hiệu suất
 app.use(compression());
 
 //init routes
 app.get('/', (req, res, next) => {
+    const strCompress = "Hello World";
     res.status(200).json({ 
-        message: 'Hello World' 
+        message: 'Hello World',
     });
 })
 
---
+++ src/dbs/init.mongodb.lv0.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { errors } = require('web3');
+const connectDB = `mongodb://localhost:27017/api_edu_dev`;
+
+mongoose.connect(connectDB).then( _ => console.log('MongoDB connected successfully'))
+.catch(err => console.log('Connection error: ', err));
+
+//dev
+if(1 === 0){
+    mongoose.set('debug', true);
+    mongoose.set('debug', { color: true });
+}
+
+module.exports = mongoose;
+
